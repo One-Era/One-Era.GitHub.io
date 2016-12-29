4679d8d9120c5ee74825d09da33a03d218ae6314
Gary Zhang <zhangjrgary@users.noreply.github.com>
2016-12-29 14:04:34 +0800

    Create common.js

---
+++ dist/js/common.js
@@ -0,0 +1,11 @@
+    var swiperH = new Swiper('.swiper-container-h', {
+        pagination: '.swiper-pagination-h',
+        paginationClickable: true,
+        spaceBetween: 50
+    });
+    var swiperV = new Swiper('.swiper-container-v', {
+        pagination: '.swiper-pagination-v',
+        paginationClickable: true,
+        direction: 'vertical',
+        spaceBetween: 50
+    });
